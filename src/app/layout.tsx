a3fbc0e403421c5b0ccd7ef6bd0d8db8c73783fe
cheeseburgertony <1821955020@qq.com>
2025-11-03 21:48:19 +0800

    feat: 添加@vercel/analytics依赖并在布局中集成分析功能

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import QueryClientProvider from "@/provider/QueryClientProvider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryClientProvider>{children}</QueryClientProvider>
+        <Analytics />
         <Toaster />
       </body>
     </html>
